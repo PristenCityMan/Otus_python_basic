b6081bedc7355a2cc61beee50822173638c34c72
PristenCityMan <timonovsv1@mail.ru>
2024-06-25 14:24:04 +0300

    Написал Д3

---
+++ homework_01/main.py
@@ -0,0 +1,52 @@
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+def is_prime (number):
+    if number < 2:
+        return False
+    for i in range (2,number // 2 + 1):
+        if number % i == 0:
+            return False
+    return True
+
+def power_numbers(*args):
+    """
+    функция, которая принимает N целых чисел,
+    и возвращает список квадратов этих чисел
+    >>> power_numbers(1, 2, 5, 7)
+    [1, 4, 25, 49]
+    """
+    items_sqr = []
+    for item in args:
+        items_sqr.append(item**2)
+    return items_sqr
+
+#print(power_numbers(1, 2, 5, 7))
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+
+
+def filter_numbers(numbers, param):
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+
+    >>> filter_numbers([1, 2, 3], ODD)
+    [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    [2, 4]
+    """
+    if param == ODD:
+        return list(filter(lambda x: x % 2 == 1, numbers))
+    elif param == EVEN:
+        return list(filter(lambda x: x % 2 == 0, numbers))
+    elif param == PRIME:
+        return list(filter(is_prime, numbers))
+    return None
+
+#print(filter_numbers([1, 2, 3, 4, 5, 6, 7, 8], PRIME))
