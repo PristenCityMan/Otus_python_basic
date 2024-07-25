b308cab3634c4564d661368d577f21d5d10e2169
PristenCityMan <timonovsv1@mail.ru>
2024-07-25 17:16:50 +0300

    тест3

---
+++ homework_02/car.py
@@ -0,0 +1,13 @@
+"""
+создайте класс `Car`, наследник `Vehicle`
+"""
+
+from Otus_python_basic.homework_02.base import Vehicle
+from Otus_python_basic.homework_02.engine import Engine
+
+
+class Car(Vehicle):
+    engine: Engine
+
+    def set_engine(self, new_engine: "Engine") -> "Engine":
+        self.engine = new_engine
